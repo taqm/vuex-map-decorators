ff6d065151c7750b0d1754db410671ff78cbde39
taqm <warabii.taqm@gmail.com>
2018-11-18 14:09:52 +0900

    version up

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -17,9 +17,21 @@ declare type DecoratorInterface<S, M, G, A> = (IfNotNever<S, {
 }> & IfNotNever<A, {
     Action: DecoratorMaker<A>;
 }>);
-export declare const State: <T = unknown>(key: keyof T, namesapce?: string | undefined) => VuexDecorator;
-export declare const Mutation: <T = unknown>(key: keyof T, namesapce?: string | undefined) => VuexDecorator;
-export declare const Getter: <T = unknown>(key: keyof T, namesapce?: string | undefined) => VuexDecorator;
-export declare const Action: <T = unknown>(key: keyof T, namesapce?: string | undefined) => VuexDecorator;
+declare type StateIntercepter<T> = (state: T) => any;
+declare type MutationsIntercepter<T> = {
+    <K extends keyof T>(commit: <KK = K>(key: KK, payload?: T[K]) => void, payload: any): void;
+};
+declare type StateKey<S> = keyof S | StateIntercepter<S>;
+declare type MutationKey<M> = keyof M | MutationsIntercepter<M>;
+declare type GetterKey<G> = keyof G;
+declare type ActionKey<A> = keyof A;
+declare type IState = <S>(key: StateKey<S>, ns?: string) => VuexDecorator;
+declare type IMutation = <S>(key: MutationKey<S>, ns?: string) => VuexDecorator;
+declare type IGetter = <S>(key: GetterKey<S>, ns?: string) => VuexDecorator;
+declare type IAction = <S>(key: ActionKey<S>, ns?: string) => VuexDecorator;
+export declare const State: IState;
+export declare const Mutation: IMutation;
+export declare const Getter: IGetter;
+export declare const Action: IAction;
 export declare function namespace<S = never, M = never, G = never, A = never>(n?: string): DecoratorInterface<S, M, G, A>;
 export {};
